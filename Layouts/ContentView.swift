44f028ea09aa4c8be0c9b42b633d4acf38f86ac4
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-09-15 09:18:44 -0400

    Some more stuff

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -10,43 +10,57 @@ import SwiftUI
 struct ContentView: View {
     @State private var texts = ".border(.orange, width: 2).border(.orange, width: 2).border(.orange, width: 2)"
     var body: some View {
-        
-        VStack {
-            Text("You have skills")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.white)
-                .padding()
-                .background(.marroon)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-            
-            Spacer()
-            
-            Text(texts)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .border(.orange, width: 2)
-            
-            Spacer()
-            
-            HStack {
-                Button("Awesome") {
-                    texts = "You Are Awesome!"
-                }
+        GeometryReader { Geometry in
+            VStack {
+    //            Text("You have skills")
+    //                .font(.largeTitle)
+    //                .fontWeight(.black)
+    //                .foregroundStyle(.white)
+    //                .padding()
+    //                .background(.marroon)
+    //                .clipShape(RoundedRectangle(cornerRadius: 15))
+                
                 Spacer()
-                Button("Great") {
-                    texts = "You Are Great!"
+                
+                Text(texts)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .border(.orange, width: 2)
+                
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                    .padding()
+                    .frame(width: 150.0)
+                
+                Rectangle()
+                    .fill(.indigo)
+                    .frame(width: Geometry.size.width * 2/3, height: 1)
+                
+                HStack {
+                    Button("Awesome") {
+                        texts = "You Are Awesome!"
+                    }
+                    Spacer()
+                    Button("Great") {
+                        texts = "You Are Great!"
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.indigo)
             }
-            .buttonStyle(.borderedProminent)
-            .border(.purple, width: 5)
-            .padding()
         }
         .padding()
     }
