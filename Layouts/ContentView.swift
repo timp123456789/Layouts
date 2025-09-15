b9b7d3e0b9d7055b55b7ccf3e6cd761bed5b35f6
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-09-14 23:09:22 -0400

    Background stuff

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var texts = ".border(.orange, width: 2).border(.orange, width: 2).border(.orange, width: 2)"
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("You have skills")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+                .padding()
+                .background(.marroon)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Spacer()
+            
+            Text(texts)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .border(.orange, width: 2)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    texts = "You Are Awesome!"
+                }
+                Spacer()
+                Button("Great") {
+                    texts = "You Are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .border(.purple, width: 5)
+            .padding()
         }
         .padding()
     }
